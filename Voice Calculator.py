12a5b61f9930258b59caa05494de6b4362641024
SoulFlare630 <78540832+SoulFlare630@users.noreply.github.com>
2023-02-15 20:02:42 +0530

    Made minor changes in the code
    As I have already said, I'm just getting into Python and I don't know much and so I haven't changed the functionality in any way, just the internal workings of the program.
    
    So, this modification makes use of elif and else to improve the detection of voice recognition errors - specifically, when it hears "sub tractor" instead of "subtraction" ie. mishearing.
    
    Along with that, I replaced an "ok" in the code with "hello", it won't change anything, I promise, it's just for fun.
    
    Not to mention that I had changed the order of some of the instructions that appear at the start.
    
    But the most important part is that PYTTSX3 IS NOT REQUIRED ANYMORE (at least for now)!
    
    
    Thanks and I hope you will find this little piece of code helpful.

--- Voice Calculator.py
+++ Voice Calculator.py
@@ -1,21 +1,20 @@
 import speech_recognition as sr 
-import pyttsx3
 import wordtodigits as wtd
 
 r = sr.Recognizer()
 mic = sr.Microphone()
-conv = wtd.convert(text="ok")
-engine = pyttsx3.init()
+conv = wtd.convert(text="hello")
 
 
 def remove(string_input): 
     return string_input.replace(" ", "")
 
 print("This is a voice calculator that supports simple essential calculation functions.")
-print("What operation would you like to do?")
 print("You can say 'Addition', 'Subtraction', 'Multiplication', 'Division'.")
-print("Please say large numbers as induvidual digits (for example, instead of 1 million, say 1,0,0,0,0,0,0) as it finds it hard to process them.")
+print("Please say large numbers as induvidual digits (for example, instead of 1 million, say 1,0,0,0,0,0,0) as it (Google) finds it hard to process them.")
+print("A quiet environment, reduced mic sensitivity along with a seperate microphone (if you have one) can make it easier (for Google) to recognize your voice")
 input("Press Enter to continue.")
+print("What operation would you like to do?")
 print("You may wait one second and then speak.")
 
 with mic as source:       
@@ -44,7 +43,7 @@ if operation == "addition":
     result = int(rem1) + int(rem2)
     print("The answer is " + str(result) + ".")
 
-if operation == "subtraction":
+elif operation == "subtraction":
     print("You have chosen Subtraction.")
     print("Say you first number.")
     with mic as source:       
@@ -65,7 +64,7 @@ if operation == "subtraction":
     result = int(rem1) - int(rem2)
     print("The answer is " + str(result) + ".")
 
-if operation == "multiplication":
+elif operation == "multiplication":
     print("You have chosen Multiplication.")
     print("Say you first number.")
     with mic as source:       
@@ -86,7 +85,7 @@ if operation == "multiplication":
     result = int(rem1) * int(rem2)
     print("The answer is " + str(result) + ".")
 
-if operation == "division":
+elif operation == "division":
     print("You have chosen Division.")
     print("Say you first number.")
     with mic as source:       
@@ -106,3 +105,6 @@ if operation == "division":
     print("Second number registered as " + str(rem2) + ".")
     result = int(rem1) / int(rem2)
     print("The answer is " + str(result) + ".")
+
+else:
+    print("Google misheard what you said - please go to a quiter environment and/or reduce you mic sensitivity and try again. If you have a better microphone, use that one.")
